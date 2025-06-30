d0b3c952ddeacbd108148250bf9535f382cd92ff
asj53 <91448572+asj53@users.noreply.github.com>
2025-06-30 13:45:02 +0100

    change

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,30 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI  = os.environ.get('DATABASE_URL') \
-                              or 'sqlite:///' + os.path.join(basedir, 'data.db')
+    # Check if we have RDS environment variables
+    if all(
+        [
+            os.environ.get("RDS_HOSTNAME"),
+            os.environ.get("RDS_USERNAME"),
+            os.environ.get("RDS_PASSWORD"),
+            os.environ.get("RDS_PORT"),
+            os.environ.get("RDS_DB_NAME"),
+        ]
+    ):
+        # Use RDS
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('RDS_USERNAME')}:{os.environ.get('RDS_PASSWORD')}@{os.environ.get('RDS_HOSTNAME')}:{os.environ.get('RDS_PORT')}/{os.environ.get('RDS_DB_NAME')}"
+    else:
+        # Use SQLite for local development
+        SQLALCHEMY_DATABASE_URI = (
+            f"sqlite:///{os.path.join(os.path.dirname(__file__), 'data', 'app.db')}"
+        )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SAVE_PATH = os.environ.get('SAVE_PATH') or os.path.join(basedir, 'data')
-    UI_PATH   = '/home/asj53/BOScheduling/UI/pages/plots'#os.environ.get('UI_PATH')   or os.path.join(basedir, 'ui')
-    NUM_SLOTS = int(os.environ.get('NUM_SLOTS', 24))
+    SAVE_PATH = os.environ.get("SAVE_PATH") or os.path.join(basedir, "data")
+    UI_PATH = "/home/asj53/BOScheduling/UI/pages/plots"  # os.environ.get('UI_PATH')   or os.path.join(basedir, 'ui')
+    NUM_SLOTS = int(os.environ.get("NUM_SLOTS", 24))
     CORS_ORIGINS = ["*"]
-    print('SAVE_PATH' , SAVE_PATH)
-    print('UI_PATH' , UI_PATH )
+    print("SAVE_PATH", SAVE_PATH)
+    print("UI_PATH", UI_PATH)
